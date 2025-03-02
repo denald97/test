e3dffacd9ea81c75b960c3973e49c3cd1421a49c
denald97 <den971997@gmail.com>
2025-03-02 17:59:27 +0300

    roles_error

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,12 +5,17 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './users.model';
 import { Role } from 'src/roles/roles.model';
 import { UserRoles } from 'src/roles/user-roles.model';
+import { RolesModule } from 'src/roles/roles.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   imports:[
-    SequelizeModule.forFeature([User, Role, UserRoles])
-  ]
+    SequelizeModule.forFeature([User, Role, UserRoles]),
+    RolesModule
+  ],
+  exports: [
+    UsersService,
+]
 })
 export class UsersModule {}
